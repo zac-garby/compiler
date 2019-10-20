d94837fc6fa22bdef84b6506febee8a3f5b31e23
Zac Garby <me@zacgarby.co.uk>
2019-10-20 20:15:07 +0100

    Introduce scan_while helper function

--- scan.c
+++ scan.c
@@ -4,10 +4,10 @@ static int scanner_is_eof(scanner_t *s);
 static char scanner_advance(scanner_t *s);
 static char scanner_peek(scanner_t *s);
 static int is_digit(char ch);
+static int scan_while(scanner_t *s, char *lexeme, int(*pred)(char s));
 
 int scanner_next(scanner_t *s, token_t *t) {
     char ch;
-    int i, done;
     
     while (*s->cursor == ' ' || *s->cursor == '\t' || *s->cursor == '\n') {
         scanner_advance(s);
@@ -26,18 +26,8 @@ int scanner_next(scanner_t *s, token_t *t) {
         t->type = T_LPAREN;
     } else if (ch == ')') {
         t->type = T_RPAREN;
-    } else if (is_digit(ch)) {
+    } else if (scan_while(s, t->lexeme, is_digit)) {
         t->type = T_INT;
-        
-        done = 0;
-        for (i = 1; !done; i++) {
-            if (is_digit(scanner_peek(s))) {
-                ch = scanner_advance(s);
-                t->lexeme[i] = ch;
-            } else {
-                done = 1;
-            }
-        }
     } else {
         return 0;
     }
@@ -45,6 +35,19 @@ int scanner_next(scanner_t *s, token_t *t) {
     scanner_advance(s);
     
     return 1;
+}
+
+static int scan_while(scanner_t *s, char *lexeme, int(*f)(char s)) {
+    int i, found;
+    found = i = 0;
+    
+    while (f(*s->cursor)) {
+        found = 1;
+        lexeme[i++] = *s->cursor;
+        scanner_advance(s);
+    }
+    
+    return found;
 }
 
 char *tok_type(int type) {
@@ -78,9 +81,11 @@ static char scanner_advance(scanner_t *s) {
     return *s->cursor;
 }
 
+/*
 static char scanner_peek(scanner_t *s) {
     return s->cursor[1];
 }
+*/
 
 int is_digit(char ch) {
     return '0' <= ch && ch <= '9';
