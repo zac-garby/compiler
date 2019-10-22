37f06a1c7120dee069cfffe5714d4be66feeafd5
Zac Garby <me@zacgarby.co.uk>
2019-10-22 21:57:35 +0100

    Define some AST nodes

--- ast.h
+++ ast.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "scan.h"
+
 typedef enum exprtype_t_ {
     EX_INT,
     EX_IDENT,
@@ -10,3 +12,37 @@ typedef enum stmttype_t_ {
     ST_COMPOUND,
     ST_EXPR
 } stmttype_t;
+
+typedef struct expr_t_ expr_t;
+typedef struct stmt_t_ stmt_t;
+
+/* EXPRESSIONS */
+
+typedef struct expr_infix_t_ {
+    expr_t *left, *right;
+    toktype_t op;
+} expr_infix_t;
+
+typedef struct expr_t_ {
+    exprtype_t type;
+    union {
+        int integer;
+        char *ident;
+        expr_infix_t *infix;
+    };
+} expr_t;
+
+/* STATEMENTS */
+
+typedef struct stmt_compound_t_ {
+    stmt_t *statements;
+    unsigned int amount;
+} stmt_compound_t;
+
+typedef struct stmt_t_ {
+    stmttype_t type;
+    union {
+        stmt_compound_t *compound;
+        expr_t *expr;
+    };
+} stmt_t;
